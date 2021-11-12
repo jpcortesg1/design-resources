aea6722bd3215ad1c00bcf6c53759368fc4bc399
jpcortesg1 <jpcortesg@eafit.edu.co>
2021-11-12 07:28:52 -0500

    make the page responsive

--- full-pages/motorcycle/main.js
+++ full-pages/motorcycle/main.js
@@ -1,18 +1,23 @@
+const toggle = document.querySelector(".toggle");
+const navigation = document.querySelector(".navigation");
 const slides = document.querySelectorAll(".slides");
 const dots = document.querySelectorAll(".dot");
 
-function setActive(i){
-  for(slide of slides)
-  slide.classList.remove("active");
+function setActive(i) {
+  for (slide of slides) slide.classList.remove("active");
   slides[i].classList.add("active");
 
-  for(dot of dots)
-  dot.classList.remove("active");
+  for (dot of dots) dot.classList.remove("active");
   dots[i].classList.add("active");
 }
 
-for (let i = 0; i < dots.length; i++){
-  dots[i].addEventListener('click', function () {
+for (let i = 0; i < dots.length; i++) {
+  dots[i].addEventListener("click", function () {
     setActive(i);
-  })
+  });
 }
+
+toggle.onclick = function () {
+  toggle.classList.toggle("active");
+  navigation.classList.toggle("active");
+};
